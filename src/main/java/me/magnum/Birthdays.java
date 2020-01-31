6a9bdf19ea38284a64d47e00abde02add3de583e
Richard Simpson <magnum1997@gmail.com>
2020-01-30 23:36:53 -0800

    Add command for testing config get/set
    Fix config test command and config
    
    Config command sets and reads values correctly

--- src/main/java/me/magnum/Birthdays.java
+++ src/main/java/me/magnum/Birthdays.java
@@ -1,5 +1,7 @@
 package me.magnum;
 
+import lombok.Getter;
+import me.magnum.birthdays.Command;
 import me.magnum.birthdays.SimpleConfig;
 import me.magnum.birthdays.SimpleConfigManager;
 import org.mineacademy.fo.plugin.SimplePlugin;
@@ -9,26 +11,30 @@ public class Birthdays extends SimplePlugin {
 	public Birthdays () {
 	}
 
+	@Getter
+	private static Birthdays birthdays;
 	public SimpleConfigManager manager;
-	public SimpleConfig config;
+
+	@Getter
+	public SimpleConfig mainConfig;
 
 	public SimpleConfig messages;
 
 	@Override
 	protected void onPluginStart () {
-
-
+		birthdays = this;
+		registerCommand(new Command());
 		String[] comments = {"Multiple lines", "Of nice comments", "Are supported !"};
 		String[] header = {"This is super simple", "And highly customizable", "new and fresh SimpleConfig !"};
 
 		this.manager = new SimpleConfigManager(this);
 
-		this.config = manager.getNewConfig("config.yml", header);
+		this.mainConfig = manager.getNewConfig("config.yml", header);
 		this.messages = manager.getNewConfig("misc/messages.yml");
 
-		this.config.set("path1", "value1", comments);
-		this.config.set("path2", "value2", "This is second comment !");
-		this.config.saveConfig();
+		this.mainConfig.set("path1", "value1", comments);
+		this.mainConfig.set("path2", "value2", "This is second comment !");
+		this.mainConfig.saveConfig();
 
 	}
 }
---
+++ src/main/java/me/magnum/birthdays/Command.java
@@ -0,0 +1,42 @@
+package me.magnum.birthdays;
+
+import me.magnum.Birthdays;
+import org.mineacademy.fo.command.SimpleCommand;
+import org.mineacademy.fo.plugin.SimplePlugin;
+
+import javax.annotation.CheckForNull;
+
+public class Command extends SimpleCommand {
+
+	private Birthdays plugin;
+
+	public Command () {
+		super("config");
+		setMinArguments(2);
+		setUsage("<get | set> <key> [value]");
+		setDescription("Test config class");
+		setPermission("plugin.permission");
+		setPermissionMessage("You don't have perms fo' dat yo.");
+	}
+
+	public void onCommand () {
+		String action = args[0];
+		String key = args[1];
+		plugin = Birthdays.getBirthdays();
+		SimpleConfig config = plugin.getMainConfig();
+		if (action.equalsIgnoreCase("get")) {
+			Object value = config.get(args[1]);
+			checkNotNull(value, args[1] + " is null");
+				returnTell("Value of "+key+" is "+ value.toString());
+		}
+		else if ((action.equalsIgnoreCase("set") && (args.length > 2))) {
+			config.set(args[1],args[2]);
+			config.saveConfig();
+			config.reloadConfig();
+			tell(args[1] + " is set to " + args[2]);
+		}
+		else tell(usageMessage);
+
+
+	}
+}
